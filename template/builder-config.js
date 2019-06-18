1bc3eca1474cce594d0bb951a1b34c86cc80cc98
Michał Zarach <michal.m.zarach@gmail.com>
2019-06-18 13:00:30 +0200

    cleanup build configuration

--- template/builder-config.js
+++ template/builder-config.js
@@ -1,49 +1,68 @@
-
 const ICONS_DIR = 'build/icons/';
 
 
-module.exports = {
-    "asar": false,
-    "productName": "My browser",
-    "appId": "org.michalzarach.my-browser",
-    "artifactName": "my-browser-${version}.${ext}",
-    "directories": {
-        "output": "build"
+const windowsOS = {
+    win: {
+        icon: ICONS_DIR +'win-icon.ico',
+        publisherName: 'michal'
     },
-    "files": [
-        "dist/**/*",
+
+    nsis: {
+        differentialPackage: true
+    }
+}
+
+const linuxOS = {
+    linux: {
+        icon: ICONS_DIR,
+        target: 'deb'
+    }
+};
+
+const macOS = {
+    mac: {
+        target: 'dmg',
+        icon: ICONS_DIR + 'con.icns'
+    },
+    dmg: {
+        contents: [
+            {
+                x: 410,
+                y: 150,
+                type: 'link',
+                path: '/Applications'
+            },
+            {
+                x: 130,
+                y: 150,
+                type: 'file'
+            }
+        ]
+    }
+};
+
+
+module.exports = {
+    asar: false,
+    productName: 'My browser',
+    appId: 'org.michalzarach.my-browser',
+    artifactName: 'my-browser-${version}.${ext}',
+    directories: {
+        output: 'build'
+    },
+    files: [
+        'dist/**/*',
         {
             from: 'src/resources/',
             to: 'resources/'
         }
     ],
-    "dmg": {
-        "contents": [
-            {
-                "x": 410,
-                "y": 150,
-                "type": "link",
-                "path": "/Applications"
-            },
-            {
-                "x": 130,
-                "y": 150,
-                "type": "file"
-            }
-        ]
-    },
-    "mac": {
-        "target": "dmg",
-        "icon": ICONS_DIR + "icon.icns"
-    },
-    "win": {
-        "icon": ICONS_DIR + "win-icon.ico",
-        "publisherName": "michal"
-    },
-    "linux": {
-        "icon": ICONS_DIR
-    },
-    "nsis": {
-        "differentialPackage": true
-    }
+    //default files: https://www.electron.build/configuration/contents
+    ...windowsOS,
+    ...linuxOS,
+    ...macOS
 };
+
+
+
+
