62eab303653ecd92bf9e8ade4157c9acd68dd2f8
Michał Zarach <michal.m.zarach@gmail.com>
2019-06-28 09:45:07 +0200

    update e2e setup

--- template/test/e2e/setup.js
+++ template/test/e2e/setup.js
@@ -1,26 +1,32 @@
 const path = require('path');
 const { BUILD_DIR } = require('../../.electron-nuxt/config');
 const { productName } = require('../../builder-config');
+const { name } = require('../../package');
 const fs = require('fs');
 
 
 const YELLOW = '\x1b[33m';
 const END = '\x1b[0m';
 
+let unpackedDir = '';
+let executableName = '';
 let extension = '';
-let unpackedDir = ''
+
 
 let os = process.platform;
 if (os === "darwin") {
     unpackedDir = 'mac';
+    executableName = name;
 } else if (os === "win32" || os === "win64") {
     unpackedDir = 'win-unpacked';
+    executableName = productName;
     extension = '.exe';
 } else if (os === "linux") {
     unpackedDir = 'linux-unpacked';
+    executableName = name;
 }
 
-const applicationPath = path.join(BUILD_DIR, `${unpackedDir}/${productName}${extension}`);
+const applicationPath = path.join(BUILD_DIR, `${unpackedDir}/${executableName}${extension}`);
 
 if (!fs.existsSync(applicationPath)) {
     throw new Error(`${YELLOW}[Spectron setup]: Application with path: '${applicationPath}' doesn't exist. 
