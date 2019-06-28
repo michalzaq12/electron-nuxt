f09d1accd902aa01b4f727899b0a01119d2e314b
Michał Zarach <michal.m.zarach@gmail.com>
2019-06-28 10:47:05 +0200

    travis config

--- tests/init.sh
+++ tests/init.sh
@@ -4,18 +4,27 @@ set -e
 # Scaffold boilerplate with given templateName
 cd "$PWD/tests"
 node scaffold.js "$1"
+sleep .5
 
-sleep 2
 
 # Install dependecies
 cd "$PWD/builds/$1"
 npm install
 
-# Run unit/e2e testing
-# npm test
 
-# Run webpack and build electron
+# Run webpack and build application
 npm run build
+
+
+# Configure virtual display server on linux os
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb start
+    sleep 3 # give xvfb some time to start
+fi
+# Run e2e testing
+npm run test:e2e
+
 
 # Clean up current scaffold
 cd ..
