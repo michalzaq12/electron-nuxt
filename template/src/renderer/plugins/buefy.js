9f4aa2e0f51d0adfcc31d28fcc76166801d38a4d
Michał Zarach <michal.m.zarach@gmail.com>
2019-07-04 11:55:26 +0200

    fixed handlebars helper tag

--- template/src/renderer/plugins/buefy.js
+++ template/src/renderer/plugins/buefy.js
@@ -3,5 +3,5 @@ import Buefy from 'buefy';
 import 'buefy/dist/buefy.css'
 
 Vue.use(Buefy, {
-    {{#unless_eq iconSet 'none'}} defaultIconPack: {{#if_eq iconSet 'fa5'}}'fa'{{/if_eq}}{{#if_eq iconSet 'mdi'}}'mdi'{{/if_eq}} {{/unless}}
+    {{#unless_eq iconSet 'none'}} defaultIconPack: {{#if_eq iconSet 'fa5'}}'fa'{{/if_eq}}{{#if_eq iconSet 'mdi'}}'mdi'{{/if_eq}} {{/unless_eq}}
 });
