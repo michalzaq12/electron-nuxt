2bff491297a8337f9c12487a20368f419c84a883
Michał Zarach <michal.m.zarach@gmail.com>
2019-07-04 10:29:27 +0200

    fixed handlebars helper tag

--- template/src/renderer/nuxt.config.js
+++ template/src/renderer/nuxt.config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     loading: false,
     plugins: [
-        {{#unless_eq iconSet 'none'}}{ssr: true, src: '@/plugins/icons.js'},{{/unless}}
+        {{#unless_eq iconSet 'none'}}{ssr: true, src: '@/plugins/icons.js'},{{/unless_eq}}
         {{#if_eq cssFramework 'buefy'}}{ssr: true, src: '@/plugins/buefy.js'},{{/if_eq}}
         {{#if_eq cssFramework 'vuetify'}}{ssr: true, src: '@/plugins/vuetify.js'},{{/if_eq}}
         {{#if_eq cssFramework 'element'}}{ssr: true, src: '@/plugins/element.js'},{{/if_eq}}
