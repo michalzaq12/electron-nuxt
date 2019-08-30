60d9eafc17ef9e43ffa76af8b211dc172e0477a6
Michał Zarach <michal.m.zarach@gmail.com>
2019-08-30 19:39:27 +0200

    update hbs helpers

--- tests/vue-cli-filters.js
+++ tests/vue-cli-filters.js
@@ -3,11 +3,11 @@ module.exports = {
   'src/renderer/pages/test/**/*': 'isCIServer',
 
   'src/renderer/pages/test/css-framework/**/*': TEST_SUITE === 'css_frameworks',
-  'test/e2e/spec/css-framework.spec.js': TEST_SUITE === 'css_frameworks',
+  'test/e2e/specs/css-framework.spec.js': TEST_SUITE === 'css_frameworks',
 
-  'src/renderer/pages/test/loader/**/*': process.env.TEST_SUITE === 'loaders',
-  'test/e2e/spec/loader.spec.js': process.env.TEST_SUITE === 'loaders',
+  'src/renderer/pages/test/loader/**/*': TEST_SUITE === 'loaders',
+  'test/e2e/specs/loader.spec.js': TEST_SUITE === 'loaders',
 
-  'src/renderer/pages/test/typescript/**/*': process.env.TEST_SUITE === 'typescript',
+  'src/renderer/pages/test/typescript/**/*': TEST_SUITE === 'typescript',
   //TODO add e2e typescript test
 }
