aac86c5c4f65fd515e8ff6b8cf56304045e94ff1
Michał Zarach <michal.m.zarach@gmail.com>
2019-10-25 16:57:53 +0200

    merge user's nuxt config with base config

--- template/.electron-nuxt/renderer/nuxt.config.js
+++ template/.electron-nuxt/renderer/nuxt.config.js
@@ -4,14 +4,16 @@ const isProduction = process.env.NODE_ENV === 'production'
 const isDev = process.env.NODE_ENV === 'development'
 const path = require('path')
 const webpack = require('webpack')
+const deepmerge = require('deepmerge')
 const nodeExternals = require('webpack-node-externals')
 const resourcesPath = require('../resources-path-provider')
 const { RENDERER_PROCESS_DIR, DIST_DIR } = require('../config')
 
 const userNuxtConfig = require('../../src/renderer/nuxt.config')
+const hasExtendFunction = (userNuxtConfig.build !== undefined && userNuxtConfig.build.extend !== undefined)
 
-module.exports = {
-  ...userNuxtConfig,
+
+const baseConfig = {
   srcDir: RENDERER_PROCESS_DIR,
   rootDir: RENDERER_PROCESS_DIR,
   router: {
@@ -22,38 +24,49 @@ module.exports = {
     dir: path.join(DIST_DIR, 'renderer')
   },
   plugins: [
-    { ssr: true, src: path.join(__dirname, 'resources-plugin.js') },
-    ...(userNuxtConfig.plugins || [])
+    { ssr: true, src: path.join(__dirname, 'resources-plugin.js') }
   ],
-  build: {
-    extend (config, { isClient }) {
-      if (userNuxtConfig.build !== undefined && userNuxtConfig.build.extend !== undefined) {
-        userNuxtConfig.build.extend(...arguments) // eslint-disable-line prefer-rest-params
-      }
-
-      config.externals = [nodeExternals({
-        modulesFromFile: {
-          include: ['dependencies']
-        }
-      })]
-
-      config.target = 'electron-renderer'
-
-      config.node = {
-        __dirname: !isProduction,
-        __filename: !isProduction
-      }
-
-      if (!isDev) {
-        // absolute path to files on production (default value: '/_nuxt/')
-        config.output.publicPath = '_nuxt/'
-      }
-
-      config.plugins.push(
-        new webpack.DefinePlugin({
-          INCLUDE_RESOURCES_PATH: isClient ? resourcesPath.nuxtClient() : resourcesPath.nuxtServer()
-        })
-      )
-    }
-  }
+  build: {}
 }
+
+
+const baseExtend = (config, { isClient }) => {
+  config.externals = [nodeExternals({
+    modulesFromFile: {
+      include: ['dependencies']
+    }
+  })]
+
+  config.target = 'electron-renderer'
+
+  config.node = {
+    __dirname: !isProduction,
+    __filename: !isProduction
+  }
+
+  if (!isDev) {
+    // absolute path to files on production (default value: '/_nuxt/')
+    config.output.publicPath = '_nuxt/'
+  }
+
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      INCLUDE_RESOURCES_PATH: isClient ? resourcesPath.nuxtClient() : resourcesPath.nuxtServer()
+    })
+  )
+
+}
+
+
+if(hasExtendFunction){
+  const userExtend = userNuxtConfig.build.extend;
+  userNuxtConfig.build.extend = () => {
+    baseExtend(...arguments)// eslint-disable-line prefer-rest-params
+    userExtend(...arguments)// eslint-disable-line prefer-rest-params
+  }
+} else {
+  baseConfig.build.extend = baseExtend;
+}
+
+
+module.exports = deepmerge(baseConfig, userNuxtConfig);
