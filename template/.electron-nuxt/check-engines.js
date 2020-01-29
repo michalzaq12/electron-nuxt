7a0d7637f3255975fc1d2a8b608218a9442457e0
Michał Zarach <michal.m.zarach@gmail.com>
2020-01-29 22:22:16 +0100

    node version checker

---
+++ template/.electron-nuxt/check-engines.js
@@ -0,0 +1,68 @@
+/*
+  This module cannot contain any external libraries!
+ */
+
+const {engines} = require('../package');
+
+const RESET = "\x1b[0m";
+const FG_RED = "\x1b[31m";
+
+function checkNodeVersion(){
+  if(engines.node === undefined) return;
+  const requiredMinVersion = engines.node.replace(/[=<>]/g, '');
+  const installedVersion = process.versions.node;
+  if(compare(requiredMinVersion, installedVersion) === 1){
+    console.log(FG_RED);
+    console.log(`\tYou are running version v${installedVersion} of Node.js, which is not supported by Electron-nuxt.`);
+    console.log(`\tThe official Node.js version that is supported is ${requiredMinVersion} or greater.`);
+    console.log(RESET);
+    console.log('\n Please visit https://nodejs.org/en/ to find instructions on how to update Node.js.')
+
+    process.exit(1);
+  }else {
+    process.exit(0);
+  }
+}
+
+
+//https://stackoverflow.com/questions/6832596/how-to-compare-software-version-number-using-js-only-number
+// Return 1 if a > b
+// Return -1 if a < b
+// Return 0 if a == b
+function compare(a, b) {
+  if (a === b) {
+    return 0;
+  }
+
+  const a_components = a.split(".");
+  const b_components = b.split(".");
+
+  const len = Math.min(a_components.length, b_components.length);
+
+  // loop while the components are equal
+  for (let i = 0; i < len; i++) {
+    // A bigger than B
+    if (parseInt(a_components[i]) > parseInt(b_components[i])) {
+      return 1;
+    }
+
+    // B bigger than A
+    if (parseInt(a_components[i]) < parseInt(b_components[i])) {
+      return -1;
+    }
+  }
+
+  // If one's a prefix of the other, the longer one is greater.
+  if (a_components.length > b_components.length) {
+    return 1;
+  }
+
+  if (a_components.length < b_components.length) {
+    return -1;
+  }
+
+  // Otherwise they are the same.
+  return 0;
+}
+
+checkNodeVersion();
