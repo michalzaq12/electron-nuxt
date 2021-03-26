611042bea06844f2fa0c7e8ac2b4758b7a1a74a3
Michał Zarach <michal.m.zarach@gmail.com>
2021-03-26 11:38:29 +0100

    remove vue devtool cleanup (windows)

--- template/src/main/boot/index.dev.js
+++ template/src/main/boot/index.dev.js
@@ -1,22 +1,8 @@
 /* eslint-disable */
-import fs from 'fs'
-import path from 'path'
 import { Menu, MenuItem, app } from 'electron'
 import { ELECTRON_RELAUNCH_CODE } from '../../../.electron-nuxt/config'
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true'
 
-
-// work around https://github.com/MarshallOfSound/electron-devtools-installer/issues/122
-// which seems to be a result of https://github.com/electron/electron/issues/19468
-if (process.platform === 'win32') {
-  const appUserDataPath = app.getPath('userData');
-  const devToolsExtensionsPath = path.join(appUserDataPath, 'DevTools Extensions');
-  try {
-    fs.unlinkSync(devToolsExtensionsPath);
-  } catch (_) {
-    // don't complain if the file doesn't exist
-  }
-}
 
 app.on('ready', () => {
   const menu = Menu.getApplicationMenu()
